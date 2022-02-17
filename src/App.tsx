7f593b74d996d7060e6f9dcaa44c350cb30670de
Michael Desanker <mdesanker@gmail.com>
2022-02-16 20:54:55 -0500

    Setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<div />} />
+    </Routes>
   );
 };
 
